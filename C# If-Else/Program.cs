07a01d0e63ad76f135141eed0071a80b0f481d7b
tonioreign <tonioreign@gmail.com>
2022-04-18 12:40:56 -0700

    Practiced If-statements

---
+++ C# If-Else/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace C_Con
+{
+
+    class Program 
+    {
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("Welcome! Tickets are $5. Please insert cash");
+
+                int cash = Convert.ToInt32(Console.ReadLine());
+
+
+                if (cash <  5) 
+                {
+                    Console.WriteLine("That's not enough money.");
+                }
+                else if (cash == 5 ) {
+                    Console.WriteLine("Here is your ticket. ");
+                }
+                else {
+                    int change = cash - 5;
+                    Console.WriteLine("Here is your ticket and " + change + " dollars in change.");
+                }
+
+
+
+
+                Console.ReadKey();
+            }
+
+
+    }
+
+
+}
