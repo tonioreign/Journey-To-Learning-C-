d39354a5988146ae509d5c25259607a7f5f0407a
tonioreign <tonioreign@gmail.com>
2022-04-18 11:24:45 -0700

    Practiced WriteLine & ReadLine & ReadKey

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Practice {
+
+    class Program 
+    {
+
+        static void Main(string[] args) 
+        {
+            //     //Change the appearance
+
+            // Console.Title = "C# Practice";
+            // Console.ForegroundColor = ConsoleColor.Blue;
+            // Console.WindowHeight = 40;
+            
+            // //Get conversation going
+        
+            // Console.WriteLine("Hi, what's your name?");
+
+            // Console.ReadLine();
+
+            // Console.WriteLine("My name is Tonio Reign. \n I am an AI sent from the future to destroy man-kind");
+
+            // Console.WriteLine("What is your favorite color?");
+
+            // Console.ReadLine();
+
+            // Console.WriteLine("Cool, mine is Destruction!");
+
+            // Console.ReadKey();
+
+            Console.WriteLine("Hello, what can I do for you?");
+            Console.ReadLine();
+            Console.WriteLine("Interesting... \n What are you here for then?");
+            Console.ReadLine();
+            Console.WriteLine("Okay, right this way...");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("BANG...BANG....BANG...");
+            Console.WriteLine("You have died....");
+            Console.ReadKey();
+
+
+
+
+
+        }
+
+    }
+
+}
