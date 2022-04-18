0db65efec249f00e8ba74367ded3d1b3badfe236
tonioreign <tonioreign@gmail.com>
2022-04-18 15:52:14 -0700

    Practicing arrays

---
+++ C# Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace C_Arrays 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+
+                string[] drinks = new string[4];
+
+                Console.WriteLine("What is your four favorite drinks?");
+
+                drinks[0] = Console.ReadLine();
+                drinks[1] = Console.ReadLine();
+                drinks[2] = Console.ReadLine();
+                drinks[3] = Console.ReadLine();
+                
+
+                Array.Sort(drinks);
+
+            Console.ReadKey();
+
+        }
+    }
+}
